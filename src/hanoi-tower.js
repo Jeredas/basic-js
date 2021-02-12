f7d0b9ad4f2fa518d110855eff86ebb14e85da56
Uladzimir Kamarou <73950275+Jeredas@users.noreply.github.com>
2021-02-12 15:08:49 +0300

    fix: fix syntax

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
-  let turns = (2**disksNumber)-1;
-  let seconds = Math.floor((3600/turnsSpeed)*turns);
-  return {turns, 
-          seconds;
-         }
-
+  let result = {
+      turns: 0,
+      seconds: 0
+    }
+  result.turns = (2**disksNumber)-1;
+  result.seconds = Math.floor((3600/turnsSpeed)*turns);
+ 
+  return result 
+}
